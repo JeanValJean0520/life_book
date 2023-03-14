8176dbf18fa56769288a31e72fc93b814da9c50d
jeavaljean <jeanvaljean0520@gmail.com>
2023-03-15 00:32:59 +0900

    [add] : dart func

---
+++ Dart/bsic/function.dart
@@ -0,0 +1,11 @@
+// 기본 함수를 사용하기 위한 코드
+
+void add(int dataA, int dataB){
+  // 받아온 dataA, dataB의 합값을 print 해준다.
+  print((dataA + dataB));
+}
+
+void main(){
+  // add 함수 호출 파라미터로 3,8을 넣어준다.
+  add(3,8);
+}
