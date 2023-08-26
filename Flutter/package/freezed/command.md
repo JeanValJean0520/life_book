119dc4e131ea45df15245bb1ff1360df0a2cf47e
jeavaljean <jeanvaljean0520@gmail.com>
2023-08-26 15:32:46 +0900

    add : freezed 패키지 추가

---
+++ Flutter/package/freezed/command.md
@@ -0,0 +1,6 @@
+flutter pub add freezed_annotation
+flutter pub add --dev build_runner
+flutter pub add --dev freezed
+# if using freezed to generate fromJson/toJson, also add:
+flutter pub add json_annotation
+flutter pub add --dev json_serializable
---
+++ Flutter/package/freezed/example.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'user_info_model.freezed.dart';
+
+part 'user_info_model.g.dart';
+
+@freezed
+class UserInfoModel with _$UserInfoModel {
+  const factory UserInfoModel(
+      {@Default("") String userId,
+      @Default("") String userName,
+      @Default(null) String? userProfile}) = _UserInfoModel;
+
+  factory UserInfoModel.fromJson(Map<String, Object?> json) =>
+      _$UserInfoModelFromJson(json);
+}
